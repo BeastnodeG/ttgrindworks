d326e5c6596e603d24aa4b01bddb446d1ef75265
BeastnodeG <beastnodegaming111@gmail.com>
2025-05-04 11:59:56 -0400

    add speed options

---
+++ alder-SpeedOptions/mod_main.gd
@@ -0,0 +1,34 @@
+extends Node
+
+
+const MOD_DIR := "alder-SpeedOptions"
+const LOG_NAME := "alder-SpeedOptions:Main"
+
+var mod_dir_path := ""
+var extensions_dir_path := ""
+var translations_dir_path := ""
+
+
+func _init() -> void:
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR)
+	install_script_extensions()
+	install_script_hook_files()
+
+	# Add translations
+	add_translations()
+
+
+func install_script_extensions() -> void:
+	extensions_dir_path = mod_dir_path.path_join("extensions")
+
+func install_script_hook_files() -> void:
+	extensions_dir_path = mod_dir_path.path_join("extensions")
+	ModLoaderMod.install_script_hooks("res://objects/save_file/settings.gd", extensions_dir_path.path_join("objects/save_file/settings.hooks.gd"))
+
+
+func add_translations() -> void:
+	translations_dir_path = mod_dir_path.path_join("translations")
+
+
+func _ready() -> void:
+	pass

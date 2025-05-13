063f698a6511c6994ed582eff8598f7c806003b9
BeastnodeG <beastnodegaming111@gmail.com>
2025-05-12 21:18:03 -0400

    random toon fix

--- mods-unpacked/alder-GreenFolio/overwrites.gd
+++ mods-unpacked/alder-GreenFolio/overwrites.gd
@@ -10,8 +10,10 @@ func _init():
 	var special_items := preload("res://mods-unpacked/alder-GreenFolio/overwrites/objects/items/pools/special_items.tres")
 	var shop_rewards := preload("res://mods-unpacked/alder-GreenFolio/overwrites/objects/items/pools/shop_rewards.tres")
 	var rewards:= preload("res://mods-unpacked/alder-GreenFolio/overwrites/objects/items/pools/rewards.tres")
+	var active_items:= preload("res://mods-unpacked/alder-GreenFolio/overwrites/objects/items/pools/active_items.tres")
 	
 	everything.take_over_path("res://objects/items/pools/everything.tres")
 	special_items.take_over_path("res://objects/items/pools/special_items.tres")
 	shop_rewards.take_over_path("res://objects/items/pools/shop_rewards.tres")
 	rewards.take_over_path("res://objects/items/pools/rewards.tres")
+	active_items.take_over_path("res://objects/items/pools/active_items.tres")

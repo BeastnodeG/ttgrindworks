c9c23e6d361954255797a95962f5ea485034d99d
BeastnodeG <beastnodegaming111@gmail.com>
2025-05-30 19:44:42 -0400

    unfinished phisher implementation

--- alder-GreenFolio/extensions/objects/battle/battle_resources/status_effects/mod_cog_effects/cog_mod_phisher.gd
+++ alder-GreenFolio/extensions/objects/battle/battle_resources/status_effects/mod_cog_effects/cog_mod_phisher.gd
@@ -5,8 +5,10 @@ func apply() -> void:
 	var player_stats = manager.battle_stats[Util.get_player()]
 	for track in player_stats.gag_regeneration.keys():
 		player_stats.gag_regeneration[track] -= 1
+		print("lowered to ", player_stats.gag_regeneration[track])
 
 func cleanup() -> void:
 	var player_stats = manager.battle_stats[Util.get_player()]
 	for track in player_stats.gag_regeneration.keys():
 		player_stats.gag_regeneration[track] += 1
+		print("increased to ", player_stats.gag_regeneration[track])
--- alder-GreenFolio/extensions/objects/battle/battle_resources/status_effects/mod_cog_effects/status_effect_mod_cog.hooks.gd
+++ alder-GreenFolio/extensions/objects/battle/battle_resources/status_effects/mod_cog_effects/status_effect_mod_cog.hooks.gd
@@ -1,2 +1,20 @@
-extends Object
-#need to hook into apply - have it combine the constant list with our own list of proxy effcts
+extends Node
+
+const CUSTOM_MOD_EFFECTS: Array[StatusEffect] = [
+	preload("res://mods-unpacked/alder-GreenFolio/extensions/objects/battle/battle_resources/status_effects/resources/mod_cog_phisher.tres"),
+]
+
+func apply(chain: ModLoaderHookChain) -> void:
+	print("running mod cog effect hook")
+	var obj := chain.reference_object as StatusEffect
+
+	var all_effects: Array[StatusEffect] = []
+	var script := obj.get_script() as Script
+	all_effects = script.MOD_EFFECTS.duplicate()
+
+	all_effects += CUSTOM_MOD_EFFECTS
+
+	if not all_effects.is_empty():
+		var chosen: StatusEffect = RandomService.array_pick_random("mod_cog_effects", all_effects).duplicate()
+		chosen.target = obj.target
+		obj.manager.add_status_effect(chosen)

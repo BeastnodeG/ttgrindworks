3b3640cd50ffb64acabbba11f8708484ce0e2102
BeastnodeG <beastnodegaming111@gmail.com>
2025-08-22 02:16:50 -0400

    nuke seeded runs rework green deal description again
    seeded runs has been replaced with item giver!

--- alder-ItemGiver/mod_main.gd
+++ alder-ItemGiver/mod_main.gd
@@ -1,8 +1,8 @@
 extends Node
 
 
-const MOD_DIR := "alder-SeededRuns"
-const LOG_NAME := "alder-SeededRuns:Main"
+const MOD_DIR := "alder-ItemGiver"
+const LOG_NAME := "alder-ItemGiver:Main"
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
@@ -23,7 +23,6 @@ func install_script_extensions() -> void:
 
 func install_script_hook_files() -> void:
 	extensions_dir_path = mod_dir_path.path_join("extensions")
-	ModLoaderMod.install_script_hooks("res://scenes/title_screen/title_screen.gd", extensions_dir_path.path_join("scenes/title_screen/title_screen.hooks.gd"))
 	ModLoaderMod.install_script_hooks("res://objects/pause_menu/pause_menu.gd", extensions_dir_path.path_join("objects/pause_menu/pause_menu.hooks.gd"))
 
 

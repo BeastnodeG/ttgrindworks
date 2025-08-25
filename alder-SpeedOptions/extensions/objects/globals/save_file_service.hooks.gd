4b07262664773479b9af7019764fa40e3be693cd
BeastnodeG <beastnodegaming111@gmail.com>
2025-08-25 18:30:05 -0400

    speed options

---
+++ alder-SpeedOptions/extensions/objects/globals/save_file_service.hooks.gd
@@ -0,0 +1,20 @@
+extends Object
+
+func _ready(chain: ModLoaderHookChain) -> void:
+	var raw_idx := 0
+	var file_path := SaveFileService.SAVE_FILE_PATH + SaveFileService.SETTINGS_FILE_NAME
+	if FileAccess.file_exists(file_path):
+		var f = FileAccess.open(file_path, FileAccess.READ)
+		for line in f.get_as_text().split("\n"):
+			if line.begins_with("battle_speed_idx"):
+				raw_idx = int(line.split("=")[1].strip_edges())
+				break
+		f.close()
+
+	chain.execute_next()
+
+	if SaveFileService.settings_file:
+		var settings = SaveFileService.settings_file
+		settings.SpeedOptions = [1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 4.0, 5.0, 7.5, 10.0, 25.0, 200.00]
+		settings.battle_speed_idx = raw_idx
+		print("restored battle_speed_idx:", settings.battle_speed_idx)
--- alder-SpeedOptions/extensions/objects/save_file/settings.hooks.gd
+++ alder-SpeedOptions/extensions/objects/save_file/settings.hooks.gd
@@ -1,9 +1,11 @@
 extends Object
 
+#commented out in mod_main
+
 func sync_settings(chain: ModLoaderHookChain) -> void:
 	chain.execute_next()
 	print("doing our injection")
 	var settings = chain.reference_object
 	if 2.5 not in settings.SpeedOptions:
-		settings.SpeedOptions.append_array([2.5, 3.0, 4.0, 5.0, 7.5, 10.0])
+		settings.SpeedOptions = [1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 4.0, 5.0, 7.5, 10.0, 25.0, 200.00]
 	#chain.execute_next()

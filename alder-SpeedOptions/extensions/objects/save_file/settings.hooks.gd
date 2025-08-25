ed6f8f5e3b2fde022307305f7e2816fdd63fb283
BeastnodeG <beastnodegaming111@gmail.com>
2025-08-25 19:40:25 -0400

    lower battle speed cap again

--- alder-SpeedOptions/extensions/objects/save_file/settings.hooks.gd
+++ alder-SpeedOptions/extensions/objects/save_file/settings.hooks.gd
@@ -7,5 +7,5 @@ func sync_settings(chain: ModLoaderHookChain) -> void:
 	print("doing our injection")
 	var settings = chain.reference_object
 	if 2.5 not in settings.SpeedOptions:
-		settings.SpeedOptions = [1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 4.0, 5.0, 7.5, 10.0, 25.0, 50.0]
+		settings.SpeedOptions = [1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 4.0, 5.0, 7.5, 10.0, 25.0]
 	#chain.execute_next()
